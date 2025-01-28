86b57e5a09b7d46bf2be0b222e1f5442f4aae55a
Andhika Bayu Permana <andhika.bayupermana@gmail.com>
2025-01-28 10:24:37 +0700

    JWT Auth - Generate Token

--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"cfa-backend/auth"
 	"cfa-backend/helper"
 	"cfa-backend/user"
 	"fmt"
@@ -11,10 +12,11 @@ import (
 
 type userHandler struct {
 	userService user.Service
+	authService auth.Service
 }
 
-func NewUserHandler(userService user.Service) *userHandler {
-	return &userHandler{userService: userService}
+func NewUserHandler(userService user.Service, authService auth.Service) *userHandler {
+	return &userHandler{userService: userService, authService: authService}
 }
 
 func (h *userHandler) RegisterUser(c *gin.Context) {
@@ -39,9 +41,16 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		return
 	}
 
-	// token, err := h.jwtService.GenerateToken()
+	token, err := h.authService.GenerateToken(newUser.ID)
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
 
-	formatter := user.FormatUser(newUser, "")
+		response := helper.APIResponse("Registered account failed!", http.StatusBadRequest, "error", errorMessage)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, token)
 
 	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
 
@@ -70,7 +79,16 @@ func (h *userHandler) Login(c *gin.Context) {
 		return
 	}
 
-	formatter := user.FormatUser(loggedinUser, "tokenLogin")
+	token, errToken := h.authService.GenerateToken(loggedinUser.ID)
+	if errToken != nil {
+		errorMessage := gin.H{"errors": errToken.Error()}
+
+		response := helper.APIResponse("Login failed!", http.StatusBadRequest, "error", errorMessage)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, token)
 
 	response := helper.APIResponse("Successfuly logged in!", http.StatusOK, "success", formatter)
 
